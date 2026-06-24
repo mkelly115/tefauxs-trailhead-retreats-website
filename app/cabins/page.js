b6613f83c513ad062fd0d4662815dccb6bdfe0b6
Michael <mikek222@gmail.com>
2026-06-24 14:55:04 -0400

    testing data pull with JSON file

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,15 @@
-export default function CabinsPage() {
+export default async function CabinsPage() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
   return (
     <div>
       <h1>Hello Cabins Page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Navigation() {
   return (
     <ul>
-        <li>
-            <Link href='/'>Home</Link>
-        </li>
+      <li>
+        <Link href='/'>Home</Link>
+      </li>
       <li>
         <Link href='/cabins'>Cabins</Link>
       </li>
