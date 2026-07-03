0974d8f8866ae1a27e331f3be37986d342ab30b8
Michael <mikek222@gmail.com>
2026-07-03 14:03:17 -0400

    updated font for tailwind

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,12 @@
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
+import { Josefin_Sans } from "next/font/google";
 import "@/app/_styles/globals.css";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   // title: "Tefauxs Trailhead Retreats",
@@ -15,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className='bg-primary-950 text-primary-100 min-h-screen'>
+      <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
         <header>
           <Logo />
           <Navigation />
