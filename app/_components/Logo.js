e98766dc6de285f647ab5e9b35b7fbbf1605f292
Michael <mikek222@gmail.com>
2026-07-15 11:27:52 -0400

    new image format

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,10 +1,19 @@
 import Link from "next/link";
+import Image from "next/image";
+import logo from "@/public/logo.png";
 
 function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-4 z-10">
-      <img src="/logo.png" height="60" width="60" alt="Tefauxs Trailhead Retreats" />
-      <span className="text-xl font-semibold text-primary-100">
+    <Link href='/' className='flex items-center gap-4 z-10'>
+      {/* <Image src="/logo.png" height="60" width="60" alt="Tefauxs Trailhead Retreats" /> */}
+      <Image
+        src={logo}
+        quality={100}
+        height='60'
+        width='60'
+        alt='Tefauxs Trailhead Retreats'
+      />
+      <span className='text-xl font-semibold text-primary-100'>
         Tefauxs Trailhead Retreats
       </span>
     </Link>
