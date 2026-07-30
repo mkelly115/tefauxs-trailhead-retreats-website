87584eb14b7ced93b164caa0a4f4176e4098789b
Michael <mikek222@gmail.com>
2026-07-30 11:37:54 -0400

    massive issues fixed with getCountries due to depreciation

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,13 @@
 export const metadata = {
   title: "Account",
-}
+};
 
 export default function AccountPage() {
   return (
     <div>
-      <h1>Account Page</h1>
+      <h2 className='font-semibold text-2xl text-accent-400 mb-7'>
+        Welcome, Michael
+      </h2>
     </div>
   );
 }
